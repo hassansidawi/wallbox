694e7197c8583479440d96df24b1cdf4f210723f
hassansidawi <107073074+hassansidawi@users.noreply.github.com>
2022-06-09 10:23:56 -0300

    Update routines.py

--- routines.py
+++ routines.py
@@ -1,3 +1,5 @@
+# Exercice 1
+
 def first_repeated(list1, list2):
   res = None
   for i in list1:
@@ -6,6 +8,7 @@ def first_repeated(list1, list2):
       break
   return res
 
+# Exercice 3
 
 def count_coin_flips(seq):
   # We suppose that seq is formed by 0's and 1's
@@ -26,4 +29,48 @@ def count_coin_flips(seq):
   return count
 
 
-def
+def flip(seq, res, count):
+  if len(seq) == 0:
+    # Stop recursion
+    return res, count
+  
+  if len(res) == 0:
+    # First step
+    n0 = seq.count(0) # How many 0's
+    n1 = seq.count(1) # How many 1's
+    if n0 > n1+1 or n1 > n0+1:
+      # No solution, no tries
+      return seq, count
+    
+    idx = 0
+    if n0 == n1+1:
+      idx = seq.index(0) # Start with 0
+    elif:
+      idx = seq.index(1)
+      
+    # Transfer from seq to res
+    res.append(seq[idx])
+    seq.pop(idx)
+    if idx != 0:
+      count += 1
+      
+    return flip(seq, res, count) # recursive call
+  
+  else:
+    i = res[-1] # last element
+    if i == 0:
+      if 1 in seq:
+        idx = seq.index(1) # find the first 1
+    else:
+      if 0 in seq:
+        idx = seq.index(0) # find the first 0
+        
+    # Transfer from seq to res
+    res.append(seq[idx])
+    seq.pop(idx)
+    
+    if idx != 0:
+      count += 1
+      
+    return flip(seq, res, count) # recursive call
+    
