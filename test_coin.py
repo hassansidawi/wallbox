64e992e0f330de797c3174cd9cff92c7e9d1ae6e
hassansidawi <107073074+hassansidawi@users.noreply.github.com>
2022-06-09 14:01:42 -0300

    Update test_coin.py

--- test_coin.py
+++ test_coin.py
@@ -2,5 +2,5 @@ import pytest
 import routines
 
 @pytest.mark.parametrize('seq, count', [([0,0,1,1], 1), ([0,1,0], 0), ([0,1], 0), ([0,0,0,1,1], 2)])
-def test_count_coin(seq):
+def test_count_coin(seq, count):
   assert routines.count_coin_flips(seq) == count
