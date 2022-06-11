8598dbee4d96b475a7040eab09216c0723cf9e6d
hassansidawi <107073074+hassansidawi@users.noreply.github.com>
2022-06-11 16:48:58 -0300

    Update test_first_repeated.py

--- test_first_repeated.py
+++ test_first_repeated.py
@@ -1,6 +1,7 @@
 import pytest
 import routines
 
-@pytest.mark.parametrize('list1, list2, res', [([1], [2], None), ([2], [2], 2), ([3], [2,3], 3), ([], [], None)])
-def test_repeated(list1, list2, res)
-  assert routines.first_repeated(list1, list2) == res
+
+@pytest.mark.parametrize('list1, list2, res', [([1], [2], None), ([2], [2], 2), ([3], [2, 3], 3), ([], [], None)])
+def test_repeated(list1, list2, res):
+    assert routines.first_repeated(list1, list2) == res
