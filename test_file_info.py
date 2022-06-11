61fdd073950528d7e4da625d41bf53bb3670630c
hassansidawi <107073074+hassansidawi@users.noreply.github.com>
2022-06-11 16:44:15 -0300

    Update test_file_info.py

--- test_file_info.py
+++ test_file_info.py
@@ -1,6 +1,8 @@
 import pytest
 import get_file_info
 
-@pytest.mark.parametrize('root_dir, filename', [('D:\\SIM\\Projetos\\FG_Telem', None)])
-def test_file(root_dir, count):
-  assert get_file_info.request(root_dir) == filename
+
+# -----> CHANGE directory name to test <---------
+@pytest.mark.parametrize('root_dir, filename', [('C:\\Users\\Windows 10\\Downloads', None)])
+def test_file(root_dir, filename):
+    assert get_file_info.request(root_dir) == filename
